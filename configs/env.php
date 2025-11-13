104e498934964069e41a210a93e6b7dcd7cf320b
kiemhieu-hub <nguyenhieu4mat@gmail.com>
2025-11-13 17:54:23 +0700

    Initial commit BaseMVC project

---
+++ configs/env.php
@@ -0,0 +1,27 @@
+<?php
+
+define('BASE_URL',          'http://localhost/D%E1%BB%B1_%C3%A1n_1/BaseMVC/');
+
+define('PATH_ROOT',         __DIR__ . '/../');
+
+define('PATH_VIEW',         PATH_ROOT . 'views/');
+
+define('PATH_VIEW_MAIN',    PATH_ROOT . 'views/main.php');
+
+define('BASE_ASSETS_UPLOADS',   BASE_URL . 'assets/uploads/');
+
+define('PATH_ASSETS_UPLOADS',   PATH_ROOT . 'assets/uploads/');
+
+define('PATH_CONTROLLER',       PATH_ROOT . 'controllers/');
+
+define('PATH_MODEL',            PATH_ROOT . 'models/');
+
+define('DB_HOST',     'localhost');
+define('DB_PORT',     '3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME',     'quan_ly_tour');
+define('DB_OPTIONS', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]);
