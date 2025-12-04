fba246f566a5e4e6ac11e61840845fbd5716ad93
kiemhieu-hub <nguyenhieu4mat@gmail.com>
2025-12-04 18:29:35 +0700

    ...

--- configs/env.php
+++ configs/env.php
@@ -23,7 +23,7 @@ define('DB_PORT',     '3306');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 
-define('DB_NAME',     'tour_management');
+define('DB_NAME',     'tour_management2');
 define('DB_OPTIONS', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
