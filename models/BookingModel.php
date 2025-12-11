44047be041a229cdcb6860a0de0100cdaefaad02
kiemhieu-hub <nguyenhieu4mat@gmail.com>
2025-12-12 00:09:48 +0700

    change code confflit

--- models/BookingModel.php
+++ models/BookingModel.php
@@ -1,11 +1,7 @@
 <?php
 require_once __DIR__ . '/BaseModel.php';
 
-<<<<<<< HEAD
-class BookingModel extends DB
-=======
 class BookingModel extends BaseModel
->>>>>>> master
 {
     protected $table = 'booking';
 
@@ -18,7 +14,6 @@ class BookingModel extends BaseModel
             ORDER BY b.id DESC
         ";
         return $this->query($sql)->fetchAll();
-<<<<<<< HEAD
     }
 
     public function findById($id)
@@ -42,28 +37,3 @@ class BookingModel extends BaseModel
         $this->execute($sql, [$status, $note, $id]);
     }
 }
-=======
-    }
-
-    public function findById($id)
-    {
-        $sql = "
-            SELECT b.*, t.name AS tour_name
-            FROM booking b
-            LEFT JOIN tours t ON b.tour_id = t.id
-            WHERE b.id = ?
-        ";
-        return $this->query($sql, [$id])->fetch();
-    }
-
-    public function updateStatus($id, $status, $note = null)
-    {
-        $sql = "
-            UPDATE booking
-            SET status = ?, admin_note = ?
-            WHERE id = ?
-        ";
-        $this->execute($sql, [$status, $note, $id]);
-    }
-}
->>>>>>> master
